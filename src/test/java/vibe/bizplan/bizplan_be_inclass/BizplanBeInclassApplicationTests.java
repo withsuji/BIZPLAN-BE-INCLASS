20312e762515ea72b07ef8193f9efbb63b190ac1
suji <qkrtnwl0314@gmail.com>
2025-11-27 21:47:06 +0900

    test: add H2 test profile for unit tests
    - Add application-test.yml with H2 in-memory database
    - Configure test to use @ActiveProfiles('test')
    - Disable Flyway for tests (use hibernate ddl-auto)

--- src/test/java/vibe/bizplan/bizplan_be_inclass/BizplanBeInclassApplicationTests.java
+++ src/test/java/vibe/bizplan/bizplan_be_inclass/BizplanBeInclassApplicationTests.java
@@ -2,12 +2,13 @@ package vibe.bizplan.bizplan_be_inclass;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BizplanBeInclassApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
